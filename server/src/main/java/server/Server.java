e3bcec70b4da36e78747b806997ce7cdc2a55cb1
troyiscoding <trm63@byu.edu>
2024-02-01 20:15:30 -0700

    Set up for project 3.

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,24 @@
+package server;
+
+import spark.*;
+
+import java.nio.file.Paths;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
