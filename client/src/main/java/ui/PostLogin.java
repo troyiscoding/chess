119dc1959a30529fb6d5da02defe1b6c0320112d
troyiscoding <trm63@byu.edu>
2024-03-20 00:21:20 -0600

    Create PostLogin Files. Help function for PostLogin.

---
+++ client/src/main/java/ui/PostLogin.java
@@ -0,0 +1,38 @@
+package ui;
+
+import java.util.Arrays;
+
+import static ui.EscapeSequences.SET_TEXT_COLOR_BLUE;
+import static ui.EscapeSequences.SET_TEXT_COLOR_WHITE;
+
+public class PostLogin {
+    public static void main(String[] args) {
+        var postLogin = new PostLogin();
+        System.out.println(postLogin.eval("help"));
+
+    }
+
+    public String eval(String input) {
+        var tokens = input.toLowerCase().split(" ");
+        var cmd = (tokens.length > 0) ? tokens[0] : "help";
+        var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+        return switch (cmd) {
+            case "help" -> help(); //Displays text informing the user what actions they can take.
+            default -> help();
+        };
+    }
+
+    //Displays text informing the user what actions they can take now that they are logged in.
+    public String help() {
+        return "Available commands:\n" +
+                SET_TEXT_COLOR_BLUE + "create <NAME> -" + SET_TEXT_COLOR_WHITE + " Create a new game.\n" +
+                SET_TEXT_COLOR_BLUE + "list -" + SET_TEXT_COLOR_WHITE + " List all current games.\n" +
+                SET_TEXT_COLOR_BLUE + "join <ID> [WHITE|BLACK|<EMPTY>] -" + SET_TEXT_COLOR_WHITE + " Play a game.\n" +
+                SET_TEXT_COLOR_BLUE + "observe <ID> -" + SET_TEXT_COLOR_WHITE + " Join game as a watcher\n" +
+                SET_TEXT_COLOR_BLUE + "logout -" + SET_TEXT_COLOR_WHITE + " Logout when you are done.\n" +
+                SET_TEXT_COLOR_BLUE + "quit -" + SET_TEXT_COLOR_WHITE + " Exit the program.\n" +
+                SET_TEXT_COLOR_BLUE + "help -" + SET_TEXT_COLOR_WHITE + " Help with possible commands\n";
+    }
+
+
+}
