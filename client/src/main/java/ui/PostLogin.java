5b66947c8ecec18309427fa2b2db8092cc954b79
troyiscoding <trm63@byu.edu>
2024-03-20 21:35:15 -0600

    Built Structure of PostLogin.

--- client/src/main/java/ui/PostLogin.java
+++ client/src/main/java/ui/PostLogin.java
@@ -19,6 +19,11 @@ public class PostLogin {
         var params = Arrays.copyOfRange(tokens, 1, tokens.length);
         return switch (cmd) {
             case "help" -> help(); //Displays text informing the user what actions they can take.
+            case "logout" -> logout(); //Logs the user out and transitions to the Pre login UI.
+            case "create" -> create(params); //Prompts the user to input a game name and creates a new game.
+            case "list" -> listGames(); //Lists all the games that currently exist on the server.
+            case "join" ->
+                    joinGame(params); //Prompts the user to specify which game they want to join and what color they want to play.
             default -> help();
         };
     }
@@ -35,5 +40,53 @@ public class PostLogin {
                 SET_TEXT_COLOR_BLUE + "help -" + SET_TEXT_COLOR_WHITE + " Help with possible commands\n";
     }
 
+    //Logs out the user
+    //Calls the sercer logout API to log out the user.
+    //When successfully logged out, the client should transition to the Pre login UI.
+    public String logout() {
+        states = LoginState.SIGNED_OUT;
+        PreLoginRepl preLoginRepl = new PreLoginRepl(serverUrl);
+        preLoginRepl.run();
+        return "";
+    }
+
+    //Allows the user to input a name for the new game.
+    //Calls the server create API to create the game. -ONLY CREATES GAME
+    public String create(String... params) {
+        if (params.length >= 1) {
+            return "You have created a game.";
+        }
+        return "Expected: <game name>";
+    }
+
+    //Lists all the games that currently exist on the server.
+    //Calls the server list API to get all the game data
+    //Displays the games in a numbered list, including game name and players (not observers).
+    //The numbering for the list should be independent of the game ID.
+    public String listGames() {
+        return "List of games";
+    }
+
+    //Allows the user to specify which game they want to join and what color they want to play.
+    //They should be able to enter the number of the desired game.
+    //Your client will need to keep track of which number corresponds to which game from the last time it listed the games
+    //Calls the server join API to join the user to the game.
+    public String joinGame(String... params) {
+        if (params.length >= 1) {
+            return "You have joined a game.";
+        }
+        return "Expected: <game number> [WHITE|BLACK|<EMPTY>]";
+    }
+
+    //Allows the user to specify which game they want to observe.
+    //They should be able to enter the number of the desired game.
+    //Your client will need to keep track of which number corresponds to which game from the last time it listed the games.
+    //Calls the server join API to verify that the game exists.
+    public String observeGame(String... params) {
+        if (params.length >= 1) {
+            return "You have joined a game as an observer.";
+        }
+        return "Expected: <game number>";
+    }
 
 }
