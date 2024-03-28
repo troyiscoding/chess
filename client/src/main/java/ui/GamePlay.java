58553d0b52b57e9919b297e03057df6b55fec471
troyiscoding <trm63@byu.edu>
2024-03-28 01:03:44 -0600

    Installed Dependencies.

---
+++ client/src/main/java/ui/GamePlay.java
@@ -0,0 +1,21 @@
+package ui;
+
+public class GamePlay {
+
+    //Help - Displays text informing the user what actions hey can take
+
+    //Redraw Chess Board - Redraws the chess board upon the user's request
+
+    //Leave - Removes the user from the game (whether they are playing or observing the game).
+    //The client transitions back to the Post-Login UI.
+
+    //Make Move - Allow the user to input what move they want to make.
+    //The board is updated to reflect the result of the move.
+    //The board automatically updates on all clients in the game.
+
+    //Resign - Propts the user to confirm they want to resign.
+    //If the user confirms, they forfit the game and the game is over.
+    //Does not cause the user to leave the game.
+
+
+}
