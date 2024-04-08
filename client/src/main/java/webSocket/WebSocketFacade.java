10917ef6dd5556a24bb1f98efc5dd5983771a429
troyiscoding <trm63@byu.edu>
2024-04-08 17:35:14 -0600

    Starting code for Client Websocket.

--- client/src/main/java/webSocket/WebSocketFacade.java
+++ client/src/main/java/webSocket/WebSocketFacade.java
@@ -1,13 +1,35 @@
 package webSocket;
 
 import com.sun.nio.sctp.NotificationHandler;
-import spark.Session;
+import org.glassfish.tyrus.core.wsadl.model.Endpoint;
 
 
-public class WebSocketFacade {
+import javax.websocket.*;
+import java.net.URI;
 
-    Session session;
+
+public class WebSocketFacade extends Endpoint {
+    public javax.websocket.Session session;
+
     NotificationHandler notificationHandler;
 
+    public WebSocketFacade() throws Exception {
+        URI uri = new URI("ws://localhost:8080/connect");
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        this.session = container.connectToServer(this, uri);
+
+        this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+            public void onMessage(String message) {
+                System.out.println(message);
+            }
+        });
+    }
+
+    public void send(String msg) throws Exception {
+        this.session.getBasicRemote().sendText(msg);
+    }
+
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
 
 }
